dca61599c36797916305b6a869df93074932ee71
aaronamm <theeranitp@gmail.com>
2021-12-27 13:21:50 +0700

    Add source code from dotnet new

---
+++ src/Codesanook.NumberGuessingGame/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace Codesanook.NumberGuessingGame.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
---
+++ start-app.sh
@@ -0,0 +1,3 @@
+#! /bin/bash
+
+dotnet watch run --project ./src/Codesanook.NumberGuessingGame/Codesanook.NumberGuessingGame.csproj
